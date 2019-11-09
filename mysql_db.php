1fff6e264c678ff3d875c7a8c050762fb3e89c75
canemangiaoche <45541931+canemangiaoche@users.noreply.github.com>
2019-11-09 16:59:31 +0100

    Update mysql_db.php

--- mysql_db.php
+++ mysql_db.php
@@ -14,10 +14,10 @@
 class mysql_db {
     
   // mysql server configuration
-  private $dbhost = 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com:5432';
-  private $dbuser = 'pxumniidglccta';
-  private $dbpass = '3f647ecc8e6721e09488c516d511dbae47704f0939407950ef80e3f3d5affe3c';
-  private $dbname = 'dojv3go4tbjun';
+  private $dbhost = 'remotemysql.com:3306';
+  private $dbuser = 's2qm3Cv4Gz';
+  private $dbpass = 'pz5TzZ4oc8';
+  private $dbname = 's2qm3Cv4Gz';
   protected $mysql;
 
   public function getMysql() {
