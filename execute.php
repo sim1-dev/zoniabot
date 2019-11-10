e7b379339c0e80887ff4b575df1cde90ae2d3ca9
canemangiaoche <45541931+canemangiaoche@users.noreply.github.com>
2019-11-10 21:27:43 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -74,7 +74,7 @@ else if(strpos($text, "/viaggio") === 0)
     $destinazione = ucfirst(substr($text, 9));
     $pianeta = new Pianeta($utente->getId_utente());
     $idp = $pianeta->selectIdPianeta($destinazione);
-    //$pianeta->setId_pianeta($idp);
+    $pianeta->setId_pianeta($idp);
     $pianeta->getPianeta();
     $pianeta->setId_pianeta($idp);
     if($pianeta->getId_proprietario() == $utente->getId_utente())
